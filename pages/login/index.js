82be1ef0fd55e4442367350021da75e4664a7716
AetheryNA <mrnaaw98@gmail.com>
2021-06-10 20:18:40 +0530

    Removed document file and changed img tag to next image component

--- pages/login/index.js
+++ pages/login/index.js
@@ -11,7 +11,13 @@ const Login = () => {
 
       <div className="login">
         <div className="login__container--intro">
-          <img src="/img/crit.svg" />
+          <Image 
+            className="login__logo"
+            src="/img/crit.svg"
+            alt="Crit"
+            width={170}
+            height={80}
+          />
 
           <p className="login__slogan"> Being Social with Gamers </p>
         </div>
