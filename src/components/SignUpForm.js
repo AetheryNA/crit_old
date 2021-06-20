7b9742a5eea7aae230d71a3823ec577b71d102fa
AetheryNA <mrnaaw98@gmail.com>
2021-06-20 20:42:46 +0530

    Sign up backend, created model for database using Prisma, created API for registering users

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,15 +1,39 @@
+import { useState } from "react"
+
 const SignUpForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  
+  const userParams = {
+    username: username,
+    password: password,
+    email: email,
+  }
+  
+  const handleRegister = async (e) => {
+    e.preventDefault()
+
+    const res = await fetch(`/api/auth/registerUser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userParams)
+    })
+  }
+
   return (
     <>
-      <form action="" className="sign-up-form">
+      <form className="sign-up-form" onSubmit={handleRegister}>
         <div className="sign-up-form__input-field">
           <label htmlFor="username-sign-up">Username</label>
-          <input type="textbox" id="username-sign-up"></input>
+          <input type="textbox" id="username-sign-up" autoComplete="off" onChange={(e) => setUsername(e.target.value)}></input>
         </div>
         
         <div className="sign-up-form__input-field">
           <label htmlFor="password-sign-up">Password</label>
-          <input type="password" id="password-sign-up"></input>
+          <input type="password" id="password-sign-up" onChange={(e) => setPassword(e.target.value)}></input>
         </div>
 
         <div className="sign-up-form__input-field">
@@ -19,7 +43,7 @@ const SignUpForm = () => {
 
         <div className="sign-up-form__input-field">
           <label htmlFor="email-sign-up">Email</label>
-          <input type="email" id="email-sign-up"></input>
+          <input type="email" id="email-sign-up" autoComplete="off" onChange={(e) => setEmail(e.target.value)}></input>
         </div>
 
         <div className="sign-up-form__submit-field">
