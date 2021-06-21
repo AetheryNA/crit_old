db6f96b0eeef01bdb3514a541455538d8e76cb5a
AetheryNA <mrnaaw98@gmail.com>
2021-06-21 18:44:11 +0530

    [47] Fixed tags and removed form action on signup form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,34 +1,32 @@
 const SignUpForm = () => {
   return (
-    <>
-      <form action="" className="sign-up-form">
-        <div className="sign-up-form__input-field">
-          <label htmlFor="username-sign-up">Username</label>
-          <input type="textbox" id="username-sign-up"></input>
-        </div>
-        
-        <div className="sign-up-form__input-field">
-          <label htmlFor="password-sign-up">Password</label>
-          <input type="password" id="password-sign-up"></input>
-        </div>
+    <form className="sign-up-form">
+      <div className="sign-up-form__input-field">
+        <label htmlFor="username-sign-up">Username</label>
+        <input type="textbox" id="username-sign-up"></input>
+      </div>
+      
+      <div className="sign-up-form__input-field">
+        <label htmlFor="password-sign-up">Password</label>
+        <input type="password" id="password-sign-up"></input>
+      </div>
 
-        <div className="sign-up-form__input-field">
-          <label htmlFor="confirm-password-sign-up">Confirm Password</label>
-          <input type="password" id="confirm-password-sign-up"></input>
-        </div>
+      <div className="sign-up-form__input-field">
+        <label htmlFor="confirm-password-sign-up">Confirm Password</label>
+        <input type="password" id="confirm-password-sign-up"></input>
+      </div>
 
-        <div className="sign-up-form__input-field">
-          <label htmlFor="email-sign-up">Email</label>
-          <input type="email" id="email-sign-up"></input>
-        </div>
+      <div className="sign-up-form__input-field">
+        <label htmlFor="email-sign-up">Email</label>
+        <input type="email" id="email-sign-up"></input>
+      </div>
 
-        <div className="sign-up-form__submit-field">
-          <button type="submit" className="sign-up-form__button button">
-            <span> Sign Up </span>
-          </button>
-        </div>
-      </form>
-    </>
+      <div className="sign-up-form__submit-field">
+        <button type="submit" className="sign-up-form__button button">
+          <span> Sign Up </span>
+        </button>
+      </div>
+    </form>
   )
 }
 
