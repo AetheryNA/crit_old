0bd66865563402dbbfeac7664182e1d877a4f128
AetheryNA <mrnaaw98@gmail.com>
2021-06-23 16:53:59 +0530

    Added new icons, changed most icons to components instead

---
+++ src/components/icons/Clock.js
@@ -0,0 +1,16 @@
+const Clock = () => {
+  return (
+    <svg 
+      id="clock" 
+      xmlns="http://www.w3.org/2000/svg"
+      className="svg--clock" 
+      width="1em" 
+      height="1em" 
+      viewBox="0 0 15 15">
+        <path id="Path_10" data-name="Path 10" d="M7.5,0A7.5,7.5,0,1,0,15,7.5,7.509,7.509,0,0,0,7.5,0Zm0,14.062A6.562,6.562,0,1,1,14.062,7.5,6.57,6.57,0,0,1,7.5,14.062Z" fill="currentColor"/>
+        <path id="Path_11" data-name="Path 11" d="M208.732,83.118h-.937V88l2.95,2.95.663-.663-2.675-2.675Z" transform="translate(-200.763 -80.305)" fill="currentColor"/>
+    </svg>
+  )
+}
+
+export default Clock
