6dab10c59467efd2ef6f6ec0b86e29c01eb330ae
AetheryNA <mrnaaw98@gmail.com>
2021-06-30 14:41:10 +0530

    WIP signup preview

--- pages/api/auth/registerUser.js
+++ pages/api/auth/registerUser.js
@@ -3,13 +3,18 @@ import User from '../../../lib/models/user';
 
 const handler = nc()
   .post(async (req, res) => {
+    try {
+      const newUser = new User(req.body)
+      const registeredUser = newUser.create()
 
-    const newUser = new User(req.body)
-    const registeredUser = newUser.create()
-
-    return res.json({
-      message: "Req sent",
-    })
+      if (!res.ok)
+        return res.status(400).json({ message: "Bad request sent T.T" })
+      else
+        return res.json({ message: "Request sent no hitches :)" })
+      
+    } catch (error) {
+      return res.status(400).json({ message: error })
+    }
   })
 
 export default handler;
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,7 +3,10 @@ import { useState } from "react"
 const SignUpForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
   const [email, setEmail] = useState('')
+  const [errorPassword, setErrorPassword] = useState(null)
+  const [errorAPI, setErrorAPI] = useState(null)
   
   const userParams = {
     username: username,
@@ -14,36 +17,53 @@ const SignUpForm = () => {
   const handleRegister = async (e) => {
     e.preventDefault()
 
-    const res = await fetch(`/api/auth/registerUser`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userParams)
-    })
+    if (password === passwordConfirm) {
+      setErrorPassword(null)
+
+      const response = await fetch('/api/auth/registerUser', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userParams)
+      })
+  
+      if (!response.ok) {
+        console.log(response.status)
+        setErrorAPI("The username or email provided has already been taken.")
+      }
+    } else {
+      setErrorPassword("Passwords don't match!")
+    }
   }
 
   return (
     <>
       <form className="sign-up-form" onSubmit={handleRegister}>
+        {errorAPI || errorPassword !== null ? 
+          <div className="sign-up-form__error-pop">
+            <p> {errorAPI}{errorPassword} </p>
+          </div>
+          : null}
+
         <div className="sign-up-form__input-field">
           <label htmlFor="username-sign-up">Username</label>
-          <input type="textbox" id="username-sign-up" autoComplete="off" onChange={(e) => setUsername(e.target.value)}></input>
+          <input type="textbox" id="username-sign-up" className={errorAPI !== null ? `sign-up-form__error` : null} autoComplete="off" required onChange={(e) => setUsername(e.target.value)}></input>
         </div>
         
         <div className="sign-up-form__input-field">
           <label htmlFor="password-sign-up">Password</label>
-          <input type="password" id="password-sign-up" onChange={(e) => setPassword(e.target.value)}></input>
+          <input type="password" id="password-sign-up" required onChange={(e) => setPassword(e.target.value)}></input>
         </div>
 
         <div className="sign-up-form__input-field">
           <label htmlFor="confirm-password-sign-up">Confirm Password</label>
-          <input type="password" id="confirm-password-sign-up"></input>
+          <input type="password" id="confirm-password-sign-up" className={errorPassword !== null ? `sign-up-form__error` : null} onChange={(e) => setPasswordConfirm(e.target.value)}></input>
         </div>
 
         <div className="sign-up-form__input-field">
           <label htmlFor="email-sign-up">Email</label>
-          <input type="email" id="email-sign-up" autoComplete="off" onChange={(e) => setEmail(e.target.value)}></input>
+          <input type="email" id="email-sign-up" className={errorAPI !== null ? `sign-up-form__error` : null} autoComplete="off" required onChange={(e) => setEmail(e.target.value)}></input>
         </div>
 
         <div className="sign-up-form__submit-field">
