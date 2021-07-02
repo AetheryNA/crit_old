fa8894dd41f2000e9df0ca8040b096c3f3886e1c
AetheryNA <mrnaaw98@gmail.com>
2021-07-02 10:47:25 +0530

    Backend progression, built API and datbase connection

--- pages/api/auth/registerUser.js
+++ pages/api/auth/registerUser.js
@@ -3,13 +3,19 @@ import User from '../../../lib/models/user';
 
 const handler = nc()
   .post(async (req, res) => {
-
     const newUser = new User(req.body)
-    const registeredUser = newUser.create()
 
-    return res.json({
-      message: "Req sent",
+    await newUser.create().catch((err) => {
+      return res
+        .status(400)
+        .json({
+          message: "Something went wrong x.x", 
+          error: err
+        })
+        .end()
     })
+
+    return res.json({ message: "Request sent, no hitches along the way :)"}).end()
   })
 
 export default handler;
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,13 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const SignUpForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
   const [email, setEmail] = useState('')
+  const [errorPassword, setErrorPassword] = useState(null)
+  const [errorAPI, setErrorAPI] = useState(null)
+  const [registerUserMsg, setRegisterUserMsg] = useState(null)
   
   const userParams = {
     username: username,
@@ -13,37 +17,112 @@ const SignUpForm = () => {
   
   const handleRegister = async (e) => {
     e.preventDefault()
+    setRegisterUserMsg(null)
 
-    const res = await fetch(`/api/auth/registerUser`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userParams)
-    })
+    if (password === passwordConfirm) {
+      setErrorPassword(null)
+
+      const response = await fetch('/api/auth/registerUser', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userParams)
+      })
+
+      const responseData = await response.json()
+
+      if (!response.ok) {
+        // const responseDataError = responseData.error.target[0].toUpperCase()
+
+        // setErrorAPI(responseDataError)
+      }
+      else {
+        setErrorAPI(null)
+        setRegisterUserMsg("You have been registered! Welcome to Crit!")
+      }
+
+    } else {
+      setErrorPassword("Passwords don't match!")
+    }
   }
 
   return (
     <>
       <form className="sign-up-form" onSubmit={handleRegister}>
+        {registerUserMsg !== null ? 
+          <div className="sign-up-form__bump">
+            <p> {registerUserMsg} </p>
+          </div>
+        : null}
+
         <div className="sign-up-form__input-field">
           <label htmlFor="username-sign-up">Username</label>
-          <input type="textbox" id="username-sign-up" autoComplete="off" onChange={(e) => setUsername(e.target.value)}></input>
+          <input 
+            type="textbox" 
+            id="username-sign-up" 
+            className={errorAPI !== null ? `sign-up-form__error` : null} 
+            autoComplete="off" 
+            required 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+
+          {errorAPI === "USERNAME" ?
+            <div className="sign-up-form__input-error"> 
+              <p> <span>{errorAPI}</span> is already registered </p>
+            </div>
+            : null
+          }
+
         </div>
         
         <div className="sign-up-form__input-field">
           <label htmlFor="password-sign-up">Password</label>
-          <input type="password" id="password-sign-up" onChange={(e) => setPassword(e.target.value)}></input>
+          <input 
+            type="password" 
+            id="password-sign-up"
+            className={errorPassword !== null ? `sign-up-form__error` : null}
+            required 
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div className="sign-up-form__input-field">
           <label htmlFor="confirm-password-sign-up">Confirm Password</label>
-          <input type="password" id="confirm-password-sign-up"></input>
+          <input 
+            type="password" 
+            id="confirm-password-sign-up" 
+            className={errorPassword !== null ? `sign-up-form__error` : null} 
+            onChange={(e) => setPasswordConfirm(e.target.value)} 
+          />
+
+          {password !== passwordConfirm ?
+            <div className="sign-up-form__input-error"> 
+              <p> <span>Passwords</span> don't match </p>
+            </div>
+            : null
+          }
+
         </div>
 
         <div className="sign-up-form__input-field">
           <label htmlFor="email-sign-up">Email</label>
-          <input type="email" id="email-sign-up" autoComplete="off" onChange={(e) => setEmail(e.target.value)}></input>
+          <input 
+            type="email" 
+            id="email-sign-up" 
+            className={errorAPI !== null ? `sign-up-form__error` : null} 
+            autoComplete="off" 
+            required 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+
+          {errorAPI === "EMAIL" ?
+            <div className="sign-up-form__input-error"> 
+              <p> <span>{errorAPI}</span> is already registered </p>
+            </div>
+            : null
+          }
+
         </div>
 
         <div className="sign-up-form__submit-field">
