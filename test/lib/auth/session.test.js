5954ef359c139031e35ed2634d3c221cfdf2411c
AetheryNA <mrnaaw98@gmail.com>
2021-07-20 01:15:13 +0530

    Login feature tests, added test env variables and modified the jest config

---
+++ test/lib/auth/session.test.js
@@ -0,0 +1,17 @@
+import { expect, jest } from "@jest/globals"
+import { ironSession } from "next-iron-session";
+
+describe("Iron session", () => {
+  jest.mock("next-iron-session")
+  jest.mock("next-connect")
+
+  test("Check if session is created", () => {
+    expect(
+      ironSession({
+        cookieName: "crit-session",
+        password: process.env.SECRET_COOKIE_PASSWORD,
+      })
+    ).toBeDefined()
+  })
+})
+
