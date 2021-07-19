fd95eef46b3b66ab215512e6e95503010e936e50
AetheryNA <mrnaaw98@gmail.com>
2021-07-20 01:14:05 +0530

    Login feature, initialized the session through iron-session, used SWR to keep user in a global state so it can be read everywhere

---
+++ pages/api/auth/user.js
@@ -0,0 +1,25 @@
+import nc from 'next-connect'
+import withSession from '../../../lib/auth/session'
+
+const handler = nc()
+  .use(withSession)
+  .get(async(req, res) => {
+    try {
+      const user = req.session.get('user')
+
+      if (user) {
+        return res.json({
+          LoggedIn: true,
+          ...user
+        })
+      } else {
+        return res.json({
+          LoggedIn: false
+        })
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  })
+
+export default handler;
