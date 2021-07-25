94a01dd4e93907c52b49dd50415a51a52bf31990
AetheryNA <mrnaaw98@gmail.com>
2021-07-25 16:57:26 +0530

    Test file changed to match new prisma client init

--- test/lib/adapters/client.test.js
+++ test/lib/adapters/client.test.js
@@ -1,8 +1,8 @@
 import { jest, expect } from '@jest/globals'
-import prisma from '../../../lib/adapters/client'
+import prisma from '../../../lib/adapters/prismaClient'
 
 describe('Prisma Client', () => {
-  jest.mock('../../../lib/adapters/client')
+  jest.mock('../../../lib/adapters/prismaClient')
 
   test('should instantiate the Prisma Client', () => {
     expect(prisma).toBeTruthy()
