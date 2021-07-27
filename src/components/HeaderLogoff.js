c08cf58269155a33bbc527c810dd1ed998190c75
AetheryNA <mrnaaw98@gmail.com>
2021-07-27 12:55:45 +0530

    Log off user functionality

--- src/components/HeaderLogoff.js
+++ src/components/HeaderLogoff.js
@@ -1,8 +1,25 @@
 import LogoffSVG from "../../public/img/icons/logoff.svg"
+import useUser from "../../lib/auth/useUser"
+import { useRouter } from "next/router"
 
 const HeaderLogoff = () => {
+  const { mutateUser } = useUser()
+  const router = useRouter()
+
+  const logoff = async (e) => {
+    e.preventDefault()
+
+    mutateUser (
+      await fetch("/api/auth/logoffUser", { 
+        method: "POST" 
+      }),
+      false,
+    );
+    router.push("/login");
+  }
+
   return (
-    <button className="log-off">
+    <button onClick={ logoff } className="log-off">
       <LogoffSVG />
     </button>
   )
