12b8ac4139f46411836022ffe604f1cc7ab4f027
AetheryNA <mrnaaw98@gmail.com>
2021-08-09 17:33:07 +0530

    Post api with image upload to local server :smile:

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
 import Head from 'next/head'
-import useUser from '../lib/auth/useUser'
-import { useEffect } from 'react'
-import { useRouter } from "next/router"
 
 const Home = () => {
-  const { user } = useUser()
-  const router = useRouter()
-
-  useEffect(() => {
-    if(!user?.LoggedIn) {
-      router.push('/trending')
-    }
-  })
-
   return (
     <>
       <Head>
