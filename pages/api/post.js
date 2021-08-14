e88119232a7a39af5680a4be27a535b449b52b52
AetheryNA <mrnaaw98@gmail.com>
2021-08-14 18:38:35 +0530

    PR

--- pages/api/post.js
+++ pages/api/post.js
@@ -31,7 +31,6 @@ const handler = nc()
     const newPost = new Post(postData)
 
     await newPost.createPost().catch((err) => {
-      console.log(err);
       return res
         .status(400)
         .json({
