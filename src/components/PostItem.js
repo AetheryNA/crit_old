295be8c74da50a878094937a532b36e25b5314e5
AetheryNA <mrnaaw98@gmail.com>
2021-08-17 11:42:52 +0530

    Get other users data and a few adjustments on other components

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -7,8 +7,6 @@ const PostItem = ({ postItems }) => {
   const postedItem = postState.map((post, index) => {
     const userData = post.author
 
-    console.log(post);
-
     return(
       <div className="post-item" key={index}>
         <div className="post-item__profile flex items-center overflow-hidden">
