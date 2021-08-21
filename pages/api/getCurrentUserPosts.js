a4f68f92932203d0aa8519ccc959689db37ff477
Naveed Ahamed <mrnaaw98@gmail.com>
2021-08-21 21:46:25 +0530

    Get current user data 📨  (#17)
    * Added a new component - Loading :clock130:
    
    * Adjustments to the dashboad and friends component styling
    
    * Get current user post data :incoming_envelope:

---
+++ pages/api/getCurrentUserPosts.js
@@ -0,0 +1,29 @@
+import nc from 'next-connect'
+import withSession from '../../lib/auth/session'
+import prisma from '../../lib/adapters/prismaClient'
+
+const handler = nc()
+  .use(withSession)
+  .get(async(req, res) => {
+    const user = req.session.get('user')
+
+    const findPosts = await prisma.posts.findMany({
+      include : {
+        author: true,
+      },
+      where : {
+        user_id: user.id,
+      }
+    }).catch((err) => {
+      res.json({
+        message: "Something went wrong",
+        err : err,
+      })
+    })
+
+    return res.json({
+      findPosts
+    })
+  })
+
+export default handler
--- src/components/UserProfileFriends.js
+++ src/components/UserProfileFriends.js
@@ -1,7 +1,7 @@
 const UserProfileFriends = () => {
   return (
     <div className="user-profile-friends">
-      <div className="user-profile-friends__friend flex flex-row justify-between">
+      <div className="user-profile-friends__friend flex flex-row">
         <a href="/" className="flex flex-row items-center">
           <img 
             src="img/pfp.png" 
