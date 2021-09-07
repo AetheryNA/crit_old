e55f132516636c064be44f8fb993aa957a1f2efb
AetheryNA <mrnaaw98@gmail.com>
2021-09-07 22:25:28 +0530

    Front end of trending page

---
+++ pages/trending/index.js
@@ -0,0 +1,32 @@
+import { withSessionSSR } from '../../lib/auth/session'
+import InnerDashboardHeader from '../../src/components/InnerdashboardHeader'
+import PostItem from '../../src/components/PostItem'
+
+const index = ({ postItems, user }) => {
+  return (
+    <>
+      <div className="dashboard-left">
+        <InnerDashboardHeader iconTrending={true} />
+        <PostItem postItems={postItems} user={user} />
+      </div>
+      <div className="dashboard-right"></div>
+    </>
+  )
+}
+
+export const getServerSideProps = withSessionSSR(async ({req, res}) => {
+  const user = req.session.get('user')
+
+  const response = await fetch('http://localhost:3000/api/getAllPosts')
+  const items = await response.json()
+  const postItems = items.findPosts
+
+  return {
+    props : {
+      postItems,
+      user
+    }
+  }
+})
+
+export default index
