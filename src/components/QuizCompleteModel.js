ff7645a54294951b9e7f1473a9d4a2865a5ff629
AetheryNA <mrnaaw98@gmail.com>
2021-09-18 10:09:07 +0530

    Pr review comments

--- src/components/QuizCompleteModel.js
+++ src/components/QuizCompleteModel.js
@@ -21,7 +21,7 @@ const QuizCompleteModel = ({ response }) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="quiz-completed__overlay fixed inset-0 transition-opacity" />
+            <Dialog.Overlay className="quiz-model__overlay fixed inset-0 transition-opacity" />
           </Transition.Child>
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
             &#8203;
@@ -36,10 +36,10 @@ const QuizCompleteModel = ({ response }) => {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="inline-block align-bottom rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="quiz-completed__wrap px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="quiz-model__wrap px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-center">
                   <div className="mt-3 text-center sm:mt-0 w-full">
-                    <Dialog.Title as="h3" className="quiz-completed__heading">
+                    <Dialog.Title as="h3" className="quiz-model__heading">
                       <p> You are a </p>
                       { response.type }
                     </Dialog.Title>
@@ -54,7 +54,7 @@ const QuizCompleteModel = ({ response }) => {
                   </div>
                 </div>
               </div>
-              <div className="quiz-completed__btn px-4 py-3 pb-4 sm:px-6 sm:flex sm:flex-row sm:items-center sm:justify-center">
+              <div className="quiz-model__btn px-4 py-3 pb-4 sm:px-6 sm:flex sm:flex-row sm:items-center sm:justify-center">
                 <button
                   type="button"
                   className="button button--primary"
